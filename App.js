7d6966556ff692bdea6a39a3a603e8afe9788e5e
bibakroy <roybibak1@gmail.com>
2023-01-18 22:56:37 +0600

    modify viewImageScreen

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
 import { Text, View } from "react-native";
 
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    // <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
-    //   <Card
-    //     title="Red jacket for sale"
-    //     subTitle="$100"
-    //     image={require("./app/assets/jacket.jpg")}
-    //   />
-    // </View>
-    <ListingDetailsScreen />
-  );
+  return <ViewImageScreen />;
 }
