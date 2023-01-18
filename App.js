efcdd915503e901e3de9113f1ee4b4cccde7865a
bibakroy <roybibak1@gmail.com>
2023-01-18 18:04:28 +0600

    display welcomeScreen in root

--- App.js
+++ App.js
@@ -4,22 +4,5 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View></View>
-    </View>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
