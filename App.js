b0873c52f1cac14106591dbf0c1129575a680bb7
bibakroy <roybibak1@gmail.com>
2023-01-19 19:44:42 +0600

    create reusable icon

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-import MessagesScreen from "./app/screens/MessagesScreen";
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
+import colors from "./app/config/colors";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={50} bgColor="red" iconColor={colors.white} />
+    </Screen>
+  );
 }
